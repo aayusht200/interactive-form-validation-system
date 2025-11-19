160ae939dd0597821e92debaf3973f2a0f0ddcf6
aayusht200 <aayush.t200@gmail.com>
2025-11-19 13:38:36 +0530

    finished half of JS logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,100 @@
-import "./style.css";
+import './style.css';
+
+const fields = {
+    email: {
+        input: document.getElementById('email'),
+        error: document.getElementById('error-email'),
+    },
+    country: {
+        input: document.getElementById('country'),
+        error: document.getElementById('error-country'),
+    },
+    postal: {
+        input: document.getElementById('postal'),
+        error: document.getElementById('error-postal'),
+    },
+    password: {
+        input: document.getElementById('password'),
+        error: document.getElementById('error-password'),
+    },
+    confirm: {
+        input: document.getElementById('confirm'),
+        error: document.getElementById('error-confirm'),
+    },
+};
+
+Object.keys(fields).forEach((key) => {
+    const { input } = fields[key];
+    input.addEventListener('input', () => {
+        formValidation(input);
+    });
+});
+function formValidation(input) {
+    switch (input.id) {
+        case 'email':
+            validateEmail(input);
+            break;
+        case 'country':
+            validateCountry(input);
+            break;
+        case 'postal':
+            validatePostal(input);
+            break;
+        case 'password':
+            validatePassword(input);
+            break;
+        case 'confirm':
+            validateConfirm(input);
+            break;
+    }
+}
+
+function validateEmail(input) {
+    const validateString = /^\S+@\S+\.\S+$/;
+    validateState(validateString, input);
+}
+
+function validateCountry(input) {
+    const validateString = /^[A-Za-z]+$/;
+    validateState(validateString, input);
+}
+
+function validatePostal(input) {
+    const validateString = /^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$/;
+    validateState(validateString, input);
+}
+
+function validatePassword(input) {
+    const validateString = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    validateState(validateString, input);
+}
+
+function validateConfirm(input) {
+    const password = fields['password'].input;
+    if (password.classList.contains('input-valid')) {
+        if (input.value === password.value) {
+            validInput(input);
+        } else {
+            invalidInput(input);
+        }
+    } else {
+        invalidInput(input);
+    }
+}
+
+function validateState(validateString, input) {
+    if (validateString.test(input.value)) {
+        validInput(input);
+    } else {
+        invalidInput(input);
+    }
+}
+
+function validInput(input) {
+    input.classList.remove('input-invalid');
+    input.classList.add('input-valid');
+}
+function invalidInput(input) {
+    input.classList.remove('input-valid');
+    input.classList.add('input-invalid');
+}
